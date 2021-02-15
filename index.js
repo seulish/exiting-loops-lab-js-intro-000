c4b1f5e6577e77faa11ae6f17ab856b898cdfb13
Leah Lee <leah@iterable.com>
2021-02-15 06:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 
-// findBy(array, findFn) which looks for a value in array based on the return value of findFn. 
-// Return null if the value isn't found. 
+// findBy(array, findFn) which looks for a value in array based on the return value of findFn.
+// Return null if the value isn't found.
 // Hint: Check the test file to know the signature of the findFn and the type of its return value
 function findBy(array, findFn) {
-  
+
+  return null
 }
