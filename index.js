57188c865ae10327d64d9ad43b14fb3d9faa8d1e
Leah Lee <leah@iterable.com>
2021-02-15 06:34:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-breakOut(array, changeValue, stopValue) {
+function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== stopValue) {
-      array[i] = changeValue
+      array[i] = changeValue;
+    }
+    else {
       break
     }
-  return array;
   }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  
 }
